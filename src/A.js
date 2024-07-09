0046c1516bbf2db05257f8236a4245be38e95038
Sowmyagangala <gangalasowmya773@gmail.com>
2024-07-09 17:24:22 +0530

    Add files via upload

---
+++ src/A.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import Conditional from './Conditional'
+import B from './B';
+import Car from './Car'
+const A = () => {
+
+  return (
+    <div>
+      <Car/>
+    </div>
+  )
+}
+export default A
